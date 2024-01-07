ca8fbe33ffba3bb2abcea6de55740e1b7c2457e9
quackquavk <138971300+quackquavk@users.noreply.github.com>
2024-01-07 07:34:10 +0545

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,10 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes:[
-    '/', '/events/:id' , 'api/webhook/clerk','api/webhook/stripe' ,'api/uploadthing'
+    '/', '/events/:id' , '/api/webhook/clerk','/api/webhook/stripe' ,'/api/uploadthing'
   ] ,
   ignoredRoutes:[
-    'api/webhook/clerk','api/webhook/stripe' ,'api/uploadthing'
+    '/api/webhook/clerk','/api/webhook/stripe' ,'/api/uploadthing'
   ]
 });
 
